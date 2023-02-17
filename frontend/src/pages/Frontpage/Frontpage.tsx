e7fd22150ca8112467a2fc9f65c63d4886ca22df
Anders Ziener Kristensen <anders.kristensen@ntnu.no>
2023-02-17 16:09:43 +0100

    Added favico and dynamic titles

--- frontend/src/pages/Frontpage/Frontpage.tsx
+++ frontend/src/pages/Frontpage/Frontpage.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import { Header } from "../../components/Header/Header";
 import { SplitSection } from "../../components/SplitSection/SplitSection";
 import { TripSection } from "../../components/TripSection/TripSection";
 import "./Frontpage.css";
 
 export const Frontpage = () => {
+  useEffect(() => {
+    document.title = "Trailmates - Hjem";
+  }, []);
   return (
     <div className="offset-container">
       <Header />
