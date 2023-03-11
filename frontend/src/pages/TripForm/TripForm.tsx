50cdc490ba6926df407dd543ed361ad73d48128d
Anders Kristensen <anders.kristensen@ntnu.no>
2023-03-11 15:17:59 +0100

    Update css and restructured form to look more appealing

--- frontend/src/pages/TripForm/TripForm.tsx
+++ frontend/src/pages/TripForm/TripForm.tsx
@@ -5,6 +5,7 @@ import { UserContext } from "../../authentication/UserProvider";
 import { uploadFile } from "../../storage/util/methods";
 import { postTrip } from "../../trips/trip";
 import { Navigate, useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
 import { LoadingIndicator } from "../../components/LoadingIndicator/LoadingIndicator";
 import { TripSubmission } from "../../types/types";
 
@@ -84,113 +85,139 @@ export const TripForm = () => {
   return (
     <form className="form" onSubmit={onSubmit}>
       <LoadingIndicator isLoading={isLoading} />
-      <div className="left-container-d">
-        <div className="title">Generelt</div>
-        <div className="field">
-          <label htmlFor="startCity">Start By</label>
-          <input
-            className="input-box"
-            id="startCity"
-            placeholder="Roma, Stavanger, Cape Town..."
-            required
-          />
+      <div className="trip-form-container">
+        <h1>Del din reiseopplevelse med andre!</h1>
+        <div
+          style={{
+            height: ".5vh",
+            width: "70%",
+            backgroundColor: "var(--accent)",
+            borderRadius: "1000px",
+            margin: "auto",
+          }}
+        />
+        <div className="trip-form-top flex-row">
+          <div className="trip-form-top-left">
+            <h2>1. Litt generelt om reisen</h2>
+            <div className="field">
+              <label htmlFor="startCity">Hvor reiste du fra?</label>
+              <input
+                className="input-box"
+                id="startCity"
+                placeholder="Roma, Stavanger, Cape Town..."
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="destinationCity">Hvor reiste du til?</label>
+              <input
+                className="input-box"
+                id="destinationCity"
+                placeholder="Firenze, Oslo, Mubishu..."
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="countries">Hvilke land var du innom?</label>
+              <input
+                className="input-box"
+                id="countries"
+                placeholder="Norge, USA, Island, osv..."
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="price">Hva kostet reisen?</label>
+              <input
+                className="input-box"
+                type="number"
+                min="0"
+                max="999999"
+                id="price"
+                placeholder="Eks. 4500, 15000,..."
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="tripDurationDays">
+                Hvor lang tid brukte du på reisen?
+              </label>
+              <input
+                className="input-box"
+                type="number"
+                min="0"
+                max="365"
+                id="tripDurationDays"
+                placeholder="Eks. 2, 3, 10..."
+                required
+              />
+            </div>
+          </div>
+          <div className="trip-form-top-right">
+            <h2>2. Legg inn noen fine bilder fra reisen din!</h2>
+            <ImageUpload {...{ setImageIds, setFiles }} />
+          </div>
         </div>
-        <div className="field">
-          <label htmlFor="destinationCity">Slutt By</label>
-          <input
-            className="input-box"
-            id="destinationCity"
-            placeholder="Firenze, Oslo, Mubishu..."
-            required
-          />
+        <div className="trip-form-middle flex-row">
+          <div className="trip-form-middle-left">
+            <h2>3. Fortell oss litt om reisen din!</h2>
+            <textarea
+              className="input-description"
+              id="description"
+              placeholder="Skriv litt om reisen din her..."
+              required
+            ></textarea>
+          </div>
+          <div className="trip-form-middle-right">
+            <h2>4. Noen få reisedetaljer til</h2>
+            <div className="field orange">
+              <label htmlFor="degreesCelcius">Klima</label>
+              <input
+                className="input-box"
+                type="number"
+                min="-100"
+                max="70"
+                step="0.1"
+                id="degreesCelcius"
+                placeholder="Forventet temperatur..."
+                required
+              />
+            </div>
+            <div className="field orange">
+              <label htmlFor="tripLengthKm">Lengde</label>
+              <input
+                className="input-box"
+                type="number"
+                min="0"
+                max="40000"
+                id="tripLengthKm"
+                placeholder="Distanse i kilometer..."
+                required
+              />
+            </div>
+            <div className="field orange">
+              <label htmlFor="attractions">Attraksjoner</label>
+              <input
+                className="input-box"
+                id="attractions"
+                placeholder="Eiffeltårnet, Sfinx, Colosseum..."
+                required
+              />
+            </div>
+          </div>
         </div>
-        <div className="field">
-          <label htmlFor="countries">Land</label>
-          <input
-            className="input-box"
-            id="countries"
-            placeholder="Norge, USA, Island, osv..."
-            required
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="price">Pris (kroner)</label>
-          <input
-            className="input-box"
-            type="number"
-            min="0"
-            max="999999"
-            id="price"
-            placeholder="Eks. 4500, 15000,..."
-            required
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="tripDurationDays">Varighet (dager)</label>
-          <input
-            className="input-box"
-            type="number"
-            min="0"
-            max="365"
-            id="tripDurationDays"
-            placeholder="Eks. 2, 3, 10..."
-            required
-          />
-        </div>
-        <div className="title">Detaljer</div>
-        <div className="field">
-          <label htmlFor="degreesCelcius">Temperatur (grader)</label>
-          <input
-            className="input-box"
-            type="number"
-            min="-100"
-            max="70"
-            step="0.1"
-            id="degreesCelcius"
-            placeholder="Forventet temperatur..."
-            required
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="tripLengthKm">Lengde (i kilometer)</label>
-          <input
-            className="input-box"
-            type="number"
-            min="0"
-            max="40000"
-            id="tripLengthKm"
-            placeholder="Distanse i kilometer..."
-            required
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="attractions">Attraksjoner (i kilometer)</label>
-          <input
-            className="input-box"
-            id="attractions"
-            placeholder="Eiffeltårnet, Sfinx, Colosseum..."
-            required
-          />
+        <div className="trip-form-container-bottom">
+          <h2>5. Publiser reisen!</h2>
+          <p>Har du husket på alt?</p>
+          <Button
+            width="30vw"
+            height="4vh"
+            text="Publiser reisen min!"
+            styling={"secondary-fill"}
+            fontSize="1.6vw"
+          ></Button>
         </div>
       </div>
-      <div className="right-container-d">
-        <div className="title">Beskrivelse</div>
-        <div className="large-field">
-          <textarea
-            className="input-description"
-            id="description"
-            placeholder="Skriv litt om reisen din her..."
-            required
-          ></textarea>
-        </div>
-        <div className="title">Bilder</div>
-        <div className="image-upload">
-          <ImageUpload {...{ setImageIds, setFiles }} />
-        </div>
-      </div>
-      <button className="submit-button" type="submit">
-        Send inn
-      </button>
     </form>
   );
 };
